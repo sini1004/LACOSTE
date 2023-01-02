7a67d464044eca42b6c8ecd41320f07f11a8b10e
shinhee <tlsgml656@gmail.com>
2023-01-02 14:57:52 +0900

    footer 스타일

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <div className="footer_wrap">
+        <div className="footer">
+          <p>
+            (주)동일드방레ᅵ대표이사 배재현ᅵ서울특별시 영등포구 여의대로 108, 파크원 타워1 31층<br/>
+            사업자등록번호 120-86-05491 사업자정보확인 ▶ᅵ통신판매업신고번호제2022-서울영등포-1744호<br/>
+            개인정보관리책임자: 송현귀 CUSTOMER SERVICE TEL 1588-9619, CS-KR@LACOSTE.COM
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
 
